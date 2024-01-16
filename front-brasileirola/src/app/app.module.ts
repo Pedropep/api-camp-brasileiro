b85ce2bc6cef925efa38f6304ceb0137200f43bc
Cb Estevão <pedroestevaopep@gmail.com>
2024-01-16 12:01:05 -0300

    Criando a base do front

--- front-brasileirola/src/app/app.module.ts
+++ front-brasileirola/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { BarraLateralComponent } from './componentes/barra-lateral/barra-lateral.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    BarraLateralComponent
   ],
   imports: [
     BrowserModule,
