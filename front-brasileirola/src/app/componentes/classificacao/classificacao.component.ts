47ba5fcb97359c6af2ef5c90f94ef98292e6a3dd
Cb Estevão <pedroestevaopep@gmail.com>
2024-01-22 16:53:40 -0300

    Trabalhando com listas de partidas por clube

--- front-brasileirola/src/app/componentes/classificacao/classificacao.component.ts
+++ front-brasileirola/src/app/componentes/classificacao/classificacao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { find } from 'rxjs';
 import { Brasileiro } from 'src/app/model/Brasileiro';
 import { Clube } from 'src/app/model/Clube';
 import { BrasileiroService } from 'src/app/service/brasileiro.service';
@@ -11,10 +12,11 @@ import { BrasileiroService } from 'src/app/service/brasileiro.service';
 export class ClassificacaoComponent implements OnInit{  
   
   partida: Brasileiro = new Brasileiro()
-  clube: Clube = new Clube()
+  clube: Clube = new Clube('',0,0,[])       
 
-  lista_all: Brasileiro[]
-  lista_clube: Clube[] = []
+  lista_all: Brasileiro[] = []
+  lista_rodada_1: Brasileiro[]
+  lista_clube: Clube[] = []  
 
   time : string
   vitorias:number = 0
@@ -27,20 +29,65 @@ export class ClassificacaoComponent implements OnInit{
   ){ }
 
   ngOnInit(): void {
+    this.criaListaClube('1')
     this.getAll()
-    // console.log(this.lista_clube)
   }
 
+  //Adiciona os nomes dos clubes da primeira rodada em uma lista de Clubes
+  criaListaClube(rodada:string){
+    this.service.getByRodada(rodada).subscribe((resp: Brasileiro[]) => {
+      this.lista_rodada_1 = resp
+        
+        this.lista_rodada_1.forEach(element => {
+          if (this.lista_rodada_1.length <= 10) {
+             this.clube = new Clube(element.mandante, 0, 0, []) 
+            this.lista_clube.push(this.clube)           
+          }if(this.lista_rodada_1.length < 11){
+             this.clube = new Clube(element.visitante, 0, 0, []) 
+            this.lista_clube.push(this.clube)         
+          }          
+        });
+        // console.log(this.lista_clube)                
+      })  
+  }
+  
   getAll(){
     this.service.getAll().subscribe((resp: Brasileiro[]) => {
-      this.lista_all = resp
-
-      this.lista_all.forEach(element => {
-        this.clube.nome = element.vencedor        
-        console.log(this.clube)
-        this.lista_clube.push(this.clube)
-      });
+      this.lista_all = resp   
+      this.montaPartidasClube()
     });
   }
-      
+
+
+  //ADICIONA TODAS AS PARTIDAS DE UM CLUBE NA LISTA DE CLUBES
+  montaPartidasClube(){    
+
+    this.lista_clube.forEach(times => {
+
+      this.lista_all.forEach(jogo => {
+        
+        const timao = this.lista_clube.find(j => j.nome === jogo.mandante && jogo.mandante === times.nome || jogo.visitante === times.nome && j.nome === jogo.visitante );             
+        
+        if(timao){            
+          this.partida.id = jogo.id
+          this.partida.vencedor = jogo.vencedor
+          this.partida.visitante = jogo.visitante
+          this.partida.mandante = jogo.mandante
+          this.partida.placar_mandante = jogo.placar_mandante
+          this.partida.rodada = jogo.rodada
+          this.partida.placar_visitante = jogo.placar_visitante
+    
+          this.partida = new Brasileiro()         
+          
+          timao.partidas.push(this.partida)          
+        }       
+      });
+    });    
+    
+    console.log(this.lista_clube)    
+  }
+  
+  calcularPontosPorRodada(rodada: number){
+    
+  }
 }
--- front-brasileirola/src/app/model/Clube.ts
+++ front-brasileirola/src/app/model/Clube.ts
@@ -1,5 +1,15 @@
+import { Brasileiro } from "./Brasileiro"
+
 export class Clube {
     public nome : string
     public qtdVitorias : number    
     public pontos : number
+    public partidas: Brasileiro[]
+
+    constructor(nome:string, qtdVitorias: number, pontos:number , partidas: Brasileiro[]){
+        this.nome = nome,
+        this.qtdVitorias = qtdVitorias,
+        this.pontos = pontos
+        this.partidas = partidas
+    }   
 }
